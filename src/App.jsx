fae2e5f108ee882a4eea9a0f329fd667d78aa840
Yuwen-0 <halil.alagul0@gmail.com>
2023-10-05 23:13:55 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Html } from '@react-three/drei'
+
+import Three from "./components/three"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Canvas id='threeCanvas'>
+
+        <Html>  
+          <h1>Three.js</h1>
+        </Html>
+        <Suspense fallback={null}>
+          <Three />
+        </Suspense>
+      </Canvas>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/three/index.jsx
@@ -0,0 +1,9 @@
+export default function Three() {
+    return (
+        <>
+            <h1>Three.js</h1>
+
+            <ambientLight/>
+        </>
+    )
+}
