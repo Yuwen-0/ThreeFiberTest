2f107e4ed7642e54533fb3e34b472c60eb7286ad
Yuwen-0 <halil.alagul0@gmail.com>
2023-10-09 00:12:29 +0300

    daily commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Html } from '@react-three/drei'
-
 import Three from "./components/three"
 
 import './App.css'
--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,71 +1,103 @@
-import { DirectionalLight } from "three";
-import { OrbitControls, PerspectiveCamera , DirectionalLightHelper } from "@react-three/drei";
-import { angleToRadians } from "../../utils/angle";
-import { useEffect , useRef } from "react";
-import { useFrame } from "@react-three/fiber";
 /* eslint-disable react/no-unknown-property */
-export default function Three() {
-    
-    const controlOrbitRef = useRef(null);
-    const boxRef = useRef(null);
+import React, { useRef } from "react";
+import { OrbitControls, PerspectiveCamera, useHelper } from "@react-three/drei";
+import { CubeTextureLoader } from "three/src/loaders/CubeTextureLoader";
+import { useThree, useLoader, useFrame } from "@react-three/fiber";
+import { useEffect } from "react";
+import { MeshDepthMaterial, PointLightHelper, SpotLightHelper } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
+import Stars from '/src/assets/img/stars.jpg';
+import Sun from '/src/assets/img/sun.jpg';
+import Mercury from "/src/assets/img/mercury.jpg";
+import Venus from "/src/assets/img/venus.jpg";
+import Earth from "/src/assets/img/earth.jpg";
 
-    useFrame((state) => {
-        if (controlOrbitRef.current){
-            const {x , y} = state.mouse;
-            controlOrbitRef.current.setAzimuthalAngle(-x * angleToRadians(45));
-            controlOrbitRef.current.setPolarAngle((y + .5) * angleToRadians(90 - 30));
-        }
+const SunAnimation = () => {
+    const { scene } = useThree();
+    const sunTexture = useLoader(TextureLoader, Sun);
+    const MercuryTexture = useLoader(TextureLoader, Mercury);
+    const VenusTexture = useLoader(TextureLoader, Venus);
+    const EarthTexture = useLoader(TextureLoader, Earth);
 
-        if(boxRef.current){
-            boxRef.current.rotation.y += 0.01
-        }
+    const sunLight = useRef(null);
+    const sunRef = useRef(null);
 
-        
-    })
+    const MercuryRef = useRef(null);
+    const MercuryParent = useRef(null);
+
+    const VenusRef = useRef(null);
+    const VenusParent = useRef(null);
+
+    const EarthRef = useRef(null);
+    const EarthParent = useRef(null);
+
     useEffect(() => {
-        if (controlOrbitRef.current){
-           controlOrbitRef.current.enableRotate = false
-           controlOrbitRef.current.enablePan = false
-           console.log(controlOrbitRef.current);
-        }
-    }, [OrbitControls.current]);
+        const cubeTextureLoader = new CubeTextureLoader();
+        const cubeTexture = cubeTextureLoader.load([
+            Stars, Stars, Stars, Stars, Stars, Stars
+        ]);
 
-        
+        scene.background = cubeTexture;
 
-    const light = new DirectionalLight(0xffffff, 1);
+        return () => {
+            scene.background = null;
+        };
+    }, [scene]);
 
+    
+    useFrame(() => {
+
+        sunRef.current.rotation.y += 0.005
+        MercuryRef.current.rotation.y += 0.0050
+        MercuryParent.current.rotation.y += 0.02
+        VenusParent.current.rotation.y += 0.009
+        VenusRef.current.rotation.y += 0.009
+        EarthParent.current.rotation.y += 0.0036
+        EarthRef.current.rotation.y += 0.0036
+    });
+
+    useHelper(sunLight, PointLightHelper, 0.5);
+
+    
     return (
         <>
-            <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-            <OrbitControls minPolarAngle={1} maxPolarAngle={2} autoRotateSpeed={0.5} autoRotate ref={controlOrbitRef} />
-            {/* Ball*/}
-            <mesh  position={[0, 1.5 , 0]}>
-                <sphereGeometry args={[1, 32, 32]} />
-                <meshStandardMaterial color="red" />
+            <PerspectiveCamera makeDefault position={[0, 50, 100]} />
+            <OrbitControls />
+            <gridHelper position={[0, -1, 0]} args={[10000, 200]} />
+
+            {/* SUN */}
+            <mesh ref={sunRef} position={[0, 0, 0]}>
+                <sphereGeometry attach="geometry" args={[16, 30, 30]} />
+                <meshBasicMaterial attach="material" map={sunTexture} />
             </mesh>
 
-            {/* Cube */}
-            <mesh ref={boxRef}  position={[0, .5, 0]}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="blue" />
-            </mesh>
+            {/* MERCURY */}
+            <Planet ParentRef={MercuryParent} PlanetRef={MercuryRef} PlanetTexture={MercuryTexture} Mass={3.2} Distance={28} />
 
-            {/* Floor */}
-            <mesh rotation={[-(angleToRadians(90)), 0, 0]} >
-                <planeGeometry args={[10, 10]}  />
-                <meshStandardMaterial color="hsl(206, 64%, 50%)" />
-            </mesh>
+            {/* VENUS */}
+            <Planet ParentRef={VenusParent} PlanetRef={VenusRef} PlanetTexture={VenusTexture} Mass={4.5} Distance={48} />
+            
 
-            {/* Light */}
-            <mesh>
-                <directionalLight args={[0xffffff, 1]}  position={[1, 1, 2.5]}/>
-            </mesh>
-            <DirectionalLightHelper args={[light, 5]} />
+            {/* EARTH */}
+            <Planet ParentRef={EarthParent} PlanetRef={EarthRef} PlanetTexture={EarthTexture} Mass={6} Distance={70} />
 
-
+            {/* LIGHTS */}
+            <pointLight ref={sunLight} distance={10000} args={["white", 4000, 1000]} />
+            
         </>
-    )
-
+    );
 }
 
+function Planet({ParentRef, PlanetRef, PlanetTexture , Mass ,Distance}) {
+    return (<mesh ref={ParentRef}>
+              <sphereGeometry attach="geometry" args={[15, 30, 30]} />
+              <mesh ref={PlanetRef} position={[Distance, 0, 0]}>
+                  <sphereGeometry attach="geometry" args={[Mass, 30, 30]} />
+                  <meshPhysicalMaterial attach="material" map={PlanetTexture} emissive="gray" emissiveIntensity={0.03} />
+                  <ambientLight distance={10} intensity={0.03} />
+              </mesh>
+          </mesh>);
+  }
+
+export default SunAnimation
